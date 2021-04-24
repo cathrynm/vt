7d5a0235c54b8304df8d8b4a8b1fd82e1f194d0d
Cathryn Mataga <cathryn@junglevision.com>
2021-04-23 19:37:33 -0700

    Blit scrolling up and down

--- src/vbxe.c
+++ src/vbxe.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
-// define some aliases for read register names
-#define CORE_VERSION        VIDEO_CONTROL
-#define MINOR_VERSION       XDL_ADR0
-#define COLDETECT           COLCLR
-#define BLT_COLLISION_CODE  BL_ADR0
-#define BLITTER_BUSY        BLITTER_START
-#define IRQ_STATUS          IRQ_CONTROL
-
-
-typedef struct _VBXE_REGS {
-    unsigned char VIDEO_CONTROL;
-    unsigned char XDL_ADR0;
+typedef struct {
+    union {
+        unsigned char VIDEO_CONTROL;
+        unsigned char CORE_VERSION;
+    };
+    union {
+        unsigned char XDL_ADR0;
+        unsigned char MINOR_REVISION;
+    };
     unsigned char XDL_ADR1;
     unsigned char XDL_ADR2;
     unsigned char CSEL;
@@ -20,13 +17,25 @@ typedef struct _VBXE_REGS {
     unsigned char CG;
     unsigned char CB;
     unsigned char COLMASK;
-    unsigned char COLCLR;
+    union {
+       unsigned char COLCLR;
+       unsigned char COLDETECT;
+    };
     unsigned char _unused0[5];
-    unsigned char BL_ADR0;
+    union {
+        unsigned char BL_ADR0;
+        unsigned char BLT_COLLISION_CODE;
+    };
     unsigned char BL_ADR1;
     unsigned char BL_ADR2;
-    unsigned char BLITTER_START;
-    unsigned char IRQ_CONTROL;
+    union {
+        unsigned char BLITTER_START;
+        volatile unsigned char BLITTER_BUSY;
+    };
+    union {
+        unsigned char IRQ_CONTROL;
+        unsigned char IRQ_STATUS;
+    };
     unsigned char P0;
     unsigned char P1;
     unsigned char P2;
@@ -36,6 +45,46 @@ typedef struct _VBXE_REGS {
     unsigned char MEMAC_CONTROL;
     unsigned char MEMAC_BANK_SEL;
 } VBXE_REGS;
+
+
+typedef struct {
+    union {
+        struct {
+            unsigned char source_adr0;
+            unsigned char source_adr1;
+        };
+        unsigned short source_adr;
+    };
+    unsigned char source_adr2;
+    unsigned char source_step_y_0;
+    unsigned char source_step_y_1;
+    unsigned char source_step_x;
+    union {
+        struct {
+            unsigned char dest_adr0;
+            unsigned char dest_adr1;
+        };
+        unsigned short dest_adr;
+    };
+    unsigned char dest_adr2;
+    unsigned char dest_step_y_0;
+    unsigned char dest_step_y_1;
+    unsigned char dest_step_x;
+    union {
+        struct {
+            unsigned char blt_width0;
+            unsigned char blt_width1;
+        };
+        unsigned short blt_width;
+    };
+    unsigned char blt_height;
+    unsigned char blt_and_mask;
+    unsigned char blt_xor_mask;
+    unsigned char blt_collison_mask;
+    unsigned char blt_zoom;
+    unsigned char pattern_feature;
+    unsigned char blt_control;
+} blitterStruct;
 
 
 //* XDL
@@ -76,13 +125,15 @@ vbxeStruct vbxe;
 
 #define XDLC    (XDLC_TMON+XDLC_RPTL+XDLC_OVADR+XDLC_CHBASE+XDLC_OVATT+XDLC_END)
 
-char xdl[] = { (XDLC % 256), (XDLC / 256), 
+static unsigned char xdl[] = { (XDLC % 256), (XDLC / 256), 
 	       215, 0x20, 0x0E, 0x00, 160, 0, 1, 1, 255 };
 
 
 unsigned char testForVbxe(void) {
+    unsigned char ver;
     vbxe.regs->CORE_VERSION = 0;
-    return vbxe.regs->CORE_VERSION == 0x10;
+    ver = vbxe.regs->CORE_VERSION;
+    return (ver == 0x10) || (ver == 0x11);
 }
 
 unsigned char vbxeTest(void)
@@ -117,11 +168,39 @@ void initVbxe(void)
     vbxe.regs->XDL_ADR2 = 0x00;
     // turn on XDL processing
     vbxe.regs->VIDEO_CONTROL = 0x01;
-    vbxe.regs->MEMAC_BANK_SEL = 0x0;      // bank zero
+    vbxe.regs->MEMAC_BANK_SEL = 0x0;
     for (y = 0;y< SCREENLINES;y++) {
         screenX.lineTab[y] = (unsigned short) y * 160;
     }
     OS.sdmctl = 0;
+}
+
+void blit(unsigned short dest, unsigned short source, unsigned char wid, unsigned char hi)
+{
+    blitterStruct *blitter = (blitterStruct *) 0x4100;
+    while (vbxe.regs->BLITTER_BUSY);
+    vbxe.regs->MEMAC_BANK_SEL = 0x80;
+    blitter->source_adr = source;
+    blitter->source_adr2 = 0;
+    blitter->source_step_y_0 = wid << 1;
+    blitter->source_step_y_1 = 0;
+    blitter->source_step_x = 1;
+    blitter->dest_adr = dest;
+    blitter->dest_adr2 = 0;
+    blitter->dest_step_y_0 = wid << 1;
+    blitter->dest_step_y_1 = 0;
+    blitter->dest_step_x = 1;
+    blitter->blt_width = wid << 1;
+    blitter->blt_height = hi - 1;
+    blitter->blt_and_mask = 0xff;
+    blitter->blt_xor_mask = 0;
+    blitter->blt_control = 0; // Next = 0 Mode = 0 (Copy)
+    blitter->blt_zoom = 0;
+    vbxe.regs->BL_ADR0 = 0;
+    vbxe.regs->BL_ADR1 = 0x1;
+    vbxe.regs->BL_ADR2 = 0;
+    vbxe.regs->BLITTER_START = 1;
+    vbxe.regs->MEMAC_BANK_SEL = 0;
 }
 
 void restoreVbxe(void)
@@ -159,10 +238,16 @@ void drawCharsAtVbxe(unsigned char *s, unsigned char len)
 
 void insertLineVbxe(unsigned char y, unsigned char yBottom)
 {
+    if (y < yBottom) {
+        blit(0x1000 + screenX.lineTab[y + 1] , 0x1000 + screenX.lineTab[y], 80, yBottom - y);
+    }
 }
 
 void deleteLineVbxe(unsigned char y, unsigned char yBottom)
 {
+     if (y < yBottom) {
+        blit(0x1000 + screenX.lineTab[y] , 0x1000 + screenX.lineTab[y+1], 80, yBottom - y);
+    }
 }
 
 void insertCharVbxe(unsigned char x, unsigned char y)
